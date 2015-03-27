12b1b117ba789d4c66edd38d12c1a128549f9b53
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2015-03-27 02:08:31 +0200

    Spark Core example

---
+++ firmware/examples/Transports/SparkCore/SparkCore.ino
@@ -0,0 +1,35 @@
+/**************************************************************
+ * Blynk is a platform with iOS and Android apps to control
+ * Arduino, Raspberry Pi and the likes over the Internet.
+ * You can easily build graphic interfaces for all your
+ * projects by simply dragging and dropping widgets.
+ *
+ *   Downloads, docs, tutorials: http://www.blynk.cc
+ *   Blynk community:            http://community.blynk.cc
+ *   Social groups:              http://www.fb.com/blynkapp
+ *                               http://twitter.com/blynk_app
+ *
+ * Blynk library is licensed under MIT license
+ * This example code is in public domain.
+ *
+ **************************************************************/
+
+//#define BLYNK_DEBUG
+#define BLYNK_PRINT Serial
+#include "blynk/BlynkSimpleSparkCore.h"
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "YourAuthToken";
+
+void setup()
+{
+  Serial.begin(9600);
+  Blynk.begin(auth);
+}
+
+void loop()
+{
+  Blynk.run();
+}
+
