479371821a8f8ca158466a5545b40dcca12b393a
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2015-03-31 14:11:32 +0300

    Enforce mode on write operations

--- firmware/BlynkApiSparkCore.h
+++ firmware/BlynkApiSparkCore.h
@@ -123,9 +123,11 @@ void BlynkApi<Proto>::processCmd(const void* buff, size_t len)
 
         if (!strcmp(cmd, "dw")) {
             //BLYNK_LOG("digitalWrite %d -> %d", pin, it.asInt());
+            pinMode(pin, OUTPUT);
             digitalWrite(pin, it.asInt() ? HIGH : LOW);
         } else if (!strcmp(cmd, "aw")) {
             //BLYNK_LOG("analogWrite %d -> %d", pin, it.asInt());
+            pinMode(pin, OUTPUT);
             analogWrite(pin, it.asInt());
         } else {
             BLYNK_LOG("Invalid HW cmd: %s", cmd);
