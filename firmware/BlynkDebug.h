89f8d8b8e01933097419ef8ace3772e1629cd83c
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2016-10-06 02:45:47 +0300

    Fixes

--- firmware/BlynkDebug.h
+++ firmware/BlynkDebug.h
@@ -52,6 +52,7 @@
 #define COUNT_OF(x) ((sizeof(x)/sizeof(0[x])) / ((size_t)(!(sizeof(x) % sizeof(0[x])))))
 #define BLYNK_ATTR_PACKED __attribute__ ((__packed__))
 #define BLYNK_NORETURN __attribute__ ((noreturn))
+#define BLYNK_UNUSED __attribute__((__unused__))
 
 // Causes problems on some platforms
 #define BLYNK_FORCE_INLINE inline //__attribute__((always_inline))
